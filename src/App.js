22150f93eb62db7647c6c52346fd1c102964fa42
thatanjan <anjan.cules@gmail.com>
2021-09-01 08:39:14 +0600

    App header and container is done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+
+import PostCard from './PostCard'
+
 function App() {
-	return <div className='App'>Insta Carousel</div>
+	return (
+		<div className='App'>
+			<Grid container>
+				<Grid
+					item
+					xs={12}
+					style={{ height: '25vh', display: 'grid', placeItems: 'center' }}
+				>
+					<Typography variant='h3'>Insta Carousel</Typography>
+				</Grid>
+
+				<Grid item container xs={12} justifyContent='center'>
+					<Grid item xs={3}>
+						<PostCard />{' '}
+					</Grid>
+				</Grid>
+			</Grid>
+		</div>
+	)
 }
 
 export default App
